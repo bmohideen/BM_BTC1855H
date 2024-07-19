2b1b463eefc47fd3034e7a2db22eb938a469df58
bmohideen <70986859+bmohideen@users.noreply.github.com>
2024-07-19 15:30:37 -0400

    Loading packages and reading data into a dataframe

--- Class Assignments/Assignment4_BilalMohideen.R
+++ Class Assignments/Assignment4_BilalMohideen.R
@@ -2,3 +2,17 @@
 #Bilal Mohideen - BTC1855H
 #R Version 2024.04.1+748 (2024.04.1+748)
 
+#loading both of the required packages for cleaning the data set
+library(tidyverse)
+library(lubridate)
+
+#importing the raw data as "ufo_data"
+ufo_data <- read.csv("ufo_subset.csv", header = TRUE)
+
+#creating a copy of the data set for data manipulation and analysis
+ufo_data1 <- ufo_data
+
+
+
+
+
